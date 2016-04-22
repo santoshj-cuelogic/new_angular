1ba94f4f294530091a3b53646124d9d818762cee
shitala-cuelogic <shital.agarwal@cuelogic.co.in>
2016-04-22 18:04:19 +0530

    Adding new route for images and changing path of images.

--- app/services/base/base.js
+++ app/services/base/base.js
@@ -58,37 +58,37 @@ function baseService($http) {
                 "name": "Ayush",
                 "department": "I.T",
                 "salary": 1000,
-                "image": "http://localhost/angular-boilerplate/assets/img/IMG_3050.JPG"
+                "image": "assets/img/IMG_3050.JPG"
             }, {
                 "id": 2,
                 "name": "Bobo",
                 "department": "Project manager",
                 "salary": 100000,
-                "image": "http://localhost/angular-boilerplate/assets/img/textures-selection-nice-high-resolution_2165080.jpg"
+                "image": "assets/img/textures-selection-nice-high-resolution_2165080.jpg"
             }, {
                 "id": 3,
                 "name": "Baby",
                 "department": "developer",
                 "salary": 2000,
-                "image": "http://localhost/angular-boilerplate/assets/img/404.png"
+                "image": "assets/img/404.png"
             }, {
                 "id": 4,
                 "name": "Nilesh",
                 "department": "Designer",
                 "salary": 5500,
-                "image": "http://localhost/angular-boilerplate/assets/img/6309_1280x800.jpg"
+                "image": "assets/img/6309_1280x800.jpg"
             }, {
                 "id": 5,
                 "name": "amol",
                 "department": "Manager",
                 "salary": 100500,
-                "image": "http://localhost/angular-boilerplate/assets/img/brand-avatar.jpg"
+                "image": "assets/img/brand-avatar.jpg"
             }, {
                 "id": 6,
                 "name": "ganesh",
                 "department": "Accountant",
                 "salary": 1000,
-                "image": "http://localhost/angular-boilerplate/assets/img/ipgeo.png"
+                "image": "assets/img/ipgeo.png"
             }]
         }
     }
